66f0295e293a722f38331b56ea21223a38fe421f
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2025-07-13 00:00:58 +0900

    Product Func Create

---
+++ app/usecase/product/save_product.go
@@ -0,0 +1,26 @@
+package usecase
+
+import domain "github.com/railgun-0402/DI-Golang/app/domain/product"
+
+type ProductUsecase struct {
+	Repo domain.ProductRepositoy
+}
+
+func NewProductUsecase(repo domain.ProductRepositoy) *ProductUsecase {
+	return &ProductUsecase{Repo: repo}
+}
+
+
+func (uc *ProductUsecase) Save(id, name, description string, price int64, stock int) (*domain.Product, error) {
+	p, err := domain.NewProduct(id, name, description, price, stock)
+	if err != nil {
+		return nil, err
+	}
+
+	err = uc.Repo.Save(p)
+	if err != nil {
+		return nil, err
+	}
+	return p, nil
+}
+
